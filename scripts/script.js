4cfe9f7fe70ab0a3d53f68755cf3eea80d719a81
tiago213s <103968515+tiago213s@users.noreply.github.com>
2022-10-03 17:04:00 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,72 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     let questions = document.querySelectorAll(".faq_boxes");
+//     // console.log(questions)
+
+//     questions.forEach(function (question) {
+//         question.addEventListener("click", handleClick);
+//     });
+// });
+
+// function handleClick (event) {
+//     console.log(event.target)
+// }
+
+let q1 = document.getElementById("q1")
+function toggle1 () {
+
+    if(q1){
+        var display = q1.style.display
+        
+        if(display == "flex"){
+            q1.style.display = "none"
+        } else {
+            q1.style.display = "flex"
+        }
+
+    }
+}
+
+let q2 = document.getElementById("q2")
+function toggle2 () {
+
+    if(q2){
+        var display = q2.style.display
+        
+        if(display == "flex"){
+            q2.style.display = "none"
+        } else {
+            q2.style.display = "flex"
+        }
+
+    }
+}
+
+let q3 = document.getElementById("q3")
+function toggle3 () {
+
+    if(q3){
+        var display = q3.style.display
+        
+        if(display == "flex"){
+            q3.style.display = "none"
+        } else {
+            q3.style.display = "flex"
+        }
+
+    }
+}
+
+let q4 = document.getElementById("q4")
+function toggle4 () {
+
+    if(q4){
+        var display = q4.style.display
+        
+        if(display == "flex"){
+            q4.style.display = "none"
+        } else {
+            q4.style.display = "flex"
+        }
+
+    }
+}
